b881b35c35735fe6afb1fd2d8c7933693fab9d2f
François <francois.roullaud@gmail.com>
2023-02-03 02:19:02 +0100

    Frames d'invulnérabilité

--- Assets/Script/EnemyPatrol.cs
+++ Assets/Script/EnemyPatrol.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Assets.Script
@@ -7,27 +8,38 @@ namespace Assets.Script
         private int _currentTarget;
         private Transform _currentTargetPosition;
 
-        public float Speed;
-        public SpriteRenderer SpriteRenderer;
-        public Transform[] TargetsPosition;
+        public const int DamageOnCollision = 20;
+
+        public float speed;
+        public SpriteRenderer spriteRenderer;
+        public Transform[] targetsPosition;
 
         // Start is called before the first frame update
         private void Start()
         {
-            _currentTargetPosition = TargetsPosition[0];
+            _currentTargetPosition = targetsPosition[0];
         }
 
         // Update is called once per frame
         private void Update()
         {
             var dir = _currentTargetPosition.position - transform.position;
-            transform.Translate(dir.normalized * Speed * Time.deltaTime, Space.World);
+            transform.Translate(speed * Time.deltaTime * dir.normalized, Space.World);
 
             if (Vector3.Distance(transform.position, _currentTargetPosition.position) < 0.3f)
             {
-                _currentTarget = (_currentTarget + 1) % TargetsPosition.Length;
-                _currentTargetPosition = TargetsPosition[_currentTarget];
-                SpriteRenderer.flipX = !SpriteRenderer.flipX;
+                _currentTarget = (_currentTarget + 1) % targetsPosition.Length;
+                _currentTargetPosition = targetsPosition[_currentTarget];
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.transform.CompareTag("Player"))
+            {
+                var playerHealth = collision.transform.GetComponent<PlayerHealth>();
+                playerHealth.TakeDamage(DamageOnCollision);
             }
         }
     }
--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,23 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Script
 {
     public class PlayerHealth : MonoBehaviour
     {
+        private const float InvicibilityFlashDelay = 0.15f;
+        private bool _tookRecentDamages;
+
         public int currentHealth;
         public HealthBar healthBar;
+
         public int maxHealth;
+
+        public SpriteRenderer spriteRenderer;
 
         private void Start()
         {
             currentHealth = maxHealth;
             healthBar.SetMaxHealth(maxHealth);
-        }
-
-        private void TakeDamage(int damage)
-        {
-            currentHealth -= damage;
-            healthBar.SetHealth(currentHealth);
         }
 
         private void Update()
@@ -26,6 +27,44 @@ namespace Assets.Script
             {
                 TakeDamage(20);
             }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (!_tookRecentDamages)
+            {
+                currentHealth -= damage;
+                healthBar.SetHealth(currentHealth);
+                HandleInvulnerabilityFrames();
+            }
+        }
+
+        private void HandleInvulnerabilityFrames()
+        {
+            StartCoroutine(WaitInvulnerabilityFrames());
+            StartCoroutine(DrawInvulnerabilityFrames());
+        }
+
+        public IEnumerator WaitInvulnerabilityFrames()
+        {
+            _tookRecentDamages = true;
+            yield return new WaitForSeconds(3f);
+            _tookRecentDamages = false;
+        }
+
+        public IEnumerator DrawInvulnerabilityFrames()
+        {
+            while (_tookRecentDamages)
+            {
+                yield return ToggleVisibility(true);
+                yield return ToggleVisibility(false);
+            }
+        }
+
+        private IEnumerator ToggleVisibility(bool visible)
+        {
+            spriteRenderer.color = visible ? new Color(1f, 1f, 1f, 0f) : new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(InvicibilityFlashDelay);
         }
     }
 }
