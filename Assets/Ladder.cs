685559fceaf953b4dcb505afcb4b3ad6e0efed4c
François <francois.roullaud@gmail.com>
2023-02-03 18:59:07 +0100

    Ladder

---
+++ Assets/Ladder.cs
@@ -0,0 +1,49 @@
+using Assets.Script;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    private bool _playerCollides;
+
+    private PlayerMovement _playerMovement;
+
+    public BoxCollider2D topCollider;
+
+    private void Awake()
+    {
+        _playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _playerCollides = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_playerMovement.isClimbing && Input.GetKeyDown(KeyCode.E))
+        {
+            _playerMovement.isClimbing = false;
+            topCollider.isTrigger = false;
+        }
+        else if (_playerCollides && Input.GetKeyDown(KeyCode.E))
+        {
+            _playerMovement.isClimbing = true;
+            topCollider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Exit");
+        if (collision.CompareTag("Player"))
+        {
+            _playerCollides = false;
+            topCollider.isTrigger = false;
+            _playerMovement.isClimbing = false;
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,85 +1,94 @@
-using Assets.Script;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Assets.Script
 {
-    public Animator animator;
-    public LayerMask collisionLayers;
-
-    public Transform groundCheck;
-    public float groundCheckRadius;
-
-    private float horizontalMovement;
-    private bool isGrounded;
-    private bool isJumping;
-
-    public float jumpForce;
-    public float moveSpeed;
-    public Rigidbody2D rb;
-    public CapsuleCollider2D playerCollider;
-
-    public SpriteRenderer spriteRenderer;
-    private Vector3 velocity = Vector3.zero;
-
-    public static PlayerMovement instance;
-
-    private void Awake()
+    public class PlayerMovement : MonoBehaviour
     {
-        if (instance != null)
+        public static PlayerMovement instance;
+
+        private float _horizontalMovement;
+        private bool _isGrounded;
+        private bool _isJumping;
+        private float _verticalMovement;
+        public Animator animator;
+        public LayerMask collisionLayers;
+
+        public Transform groundCheck;
+        public float groundCheckRadius;
+        public bool isClimbing;
+
+        public float jumpForce;
+        public float moveSpeed;
+        public CapsuleCollider2D playerCollider;
+        public Rigidbody2D rb;
+
+        public SpriteRenderer spriteRenderer;
+
+        private void Awake()
         {
-            Debug.LogWarning("Numerous PlayerMovement instances in the scene");
-            return;
+            if (instance != null)
+            {
+                Debug.LogWarning("Numerous PlayerMovement instances in the scene");
+                return;
+            }
+
+            instance = this;
         }
 
-        instance = this;
-    }
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        private void Update()
         {
-            isJumping = true;
+            if (Input.GetButtonDown("Jump") && _isGrounded)
+            {
+                _isJumping = true;
+            }
+
+            _horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
+            _verticalMovement = Input.GetAxis("Vertical") * moveSpeed;
+
+            var characterVelocity = Mathf.Abs(rb.velocity.x);
+            animator.SetFloat("Speed", characterVelocity);
         }
 
-        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed;
-
-        var characterVelocity = Mathf.Abs(rb.velocity.x);
-        animator.SetFloat("Speed", characterVelocity);
-    }
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
-        Flip(rb.velocity.x);
-        MovePlayer(horizontalMovement);
-    }
-
-    private void MovePlayer(float _horizontalMovement)
-    {
-        rb.velocity = new Vector2(_horizontalMovement, rb.velocity.y);
-
-        if (isJumping)
+        private void FixedUpdate()
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            isJumping = false;
+            _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
+            Flip(rb.velocity.x);
+            MovePlayer(_horizontalMovement, _verticalMovement);
         }
-    }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-    }
-
-    private void Flip(float _velocity)
-    {
-        if (_velocity > 0.1f)
+        private void MovePlayer(float horizontalMovement, float verticalMovement)
         {
-            spriteRenderer.flipX = false;
+            rb.velocity = new Vector2(horizontalMovement, rb.velocity.y);
+
+            if (_isJumping)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce));
+                _isJumping = false;
+            }
+
+            if (isClimbing)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, verticalMovement);
+                _isJumping = false;
+            }
         }
-        else if (_velocity < -0.1f)
+
+        private void OnDrawGizmos()
         {
-            spriteRenderer.flipX = true;
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+
+        private void Flip(float velocity)
+        {
+            if (velocity > 0.1f)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (velocity < -0.1f)
+            {
+                spriteRenderer.flipX = true;
+            }
         }
     }
 }
